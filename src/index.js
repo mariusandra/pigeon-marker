2877380dad09545ce58d2394ee1749902ca86c2f
info@suther.de <info@suther.de>
2020-11-03 14:41:33 +0100

    Fix marker-position if use customized marker-size Optimize SVG-Image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import { React, Component } from './infact'
 import PropTypes from 'prop-types'
 
-const imageOffset = {
-  left: 15,
-  top: 31
-}
-
 export default class Marker extends Component {
   static propTypes = process.env.BABEL_ENV === 'inferno' ? {} : {
     // marker
@@ -94,6 +89,10 @@ export default class Marker extends Component {
     const { hover } = this.state
     let svgSize = markerSize
 
+    const imageOffset = {
+      left: svgSize / 2,
+      top: svgSize
+    }
     const style = {
       position: 'absolute',
       transform: `translate(${left - imageOffset.left}px, ${top - imageOffset.top}px)`,
@@ -113,16 +112,52 @@ export default class Marker extends Component {
         onMouseOut={this.handleMouseOut}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          style={{width: `${svgSize}px`, height: `${svgSize}px`}}
-          viewBox="0 0 24 24">
+          viewBox="0 0 24 24"
+          style={{width: svgSize, height: svgSize}}
+        >
           <filter id="shadow">
             <feDropShadow dx="0" dy="0" stdDeviation="0.9"
               floodColor={markerShadowColor}/>
           </filter>
+          <ellipse
+            cx="11.961"
+            cy="22.541"
+            fill="#8B8B8B"
+            fillOpacity="0.8"
+            strokeOpacity="0.65"
+            paintOrder="fill"
+            rx="6.058"
+            ry="1.431"
+            bxOrigin="0.77789 -3.095136"
+          ></ellipse>
+          <path
+            fill={hover ? markerColorHover : markerColor}
+            stroke="#FFF"
+            strokeLinecap="square"
+            strokeMiterlimit="1"
+            strokeOpacity="0.9"
+            strokeWidth="2"
+            d="M12 1.017c-4.095 0-7.416 3.201-7.416 7.152 0 5.361 7.416 13.28 7.416 13.28s7.415-7.919 7.415-13.28c0-3.951-3.32-7.152-7.415-7.152z"
+            filter="url(#shadow)"
+            paintOrder="stroke"
+            bxOrigin="0.631861 0.519651"
+          ></path>
           <path
             fill={hover ? markerColorHover : markerColor}
             style={{filter: 'url(#shadow)'}}
-            d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
+            d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z"
+          />
+          <circle
+            cx="11.46"
+            cy="10.761"
+            r="2.122"
+            fill="#FFF"
+            fillOpacity="0.85"
+            strokeOpacity="0.85"
+            paintOrder="fill"
+            transform="matrix(1.2673 0 0 1.22227 -2.505 -4.988)"
+            bxOrigin="1.115508 1.285579"
+          ></circle>
         </svg>
       </div>
     )
